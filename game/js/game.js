21c1d22808d6bd77c7494ddf889164627d87c61c
Udo Schroeter <udo.schroeter@gmail.com>
2021-10-02 11:53:34 +0200

    Initial commit

---
+++ game/js/game.js
@@ -0,0 +1,29 @@
+var Game = {
+	
+	stage : false,
+	
+	debugInfo : {
+		lastRenderTime : 0,
+	},
+	
+	state : {
+		
+	},
+	
+	quick_save : () => {
+		
+	},
+	
+	quick_load : () => {
+		
+	},
+	
+	start_new : () => {
+		
+	},
+	
+	init : () => {
+				
+	},
+	
+}
